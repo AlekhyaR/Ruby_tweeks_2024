5a93aa754877dd7ac88efadee0c48fea480ef4eb
Alekhya <alekhya.gajing@gmail.com>
2024-07-07 16:44:16 +0200

    Create upcase_first.rb

---
+++ upcase_first.rb
@@ -0,0 +1,7 @@
+Be cautious when to use titleize, capitalize or upcase_first
+
+They are similar to each other, but have different implementations and performance characteristics. 
+Go through below link for better understanding. 
+
+reference link 
+https://igor.works/blog/rails_string_upcase_first_vs_capitalize_and_titleize
